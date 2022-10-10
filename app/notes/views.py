327687f9981f2406ca464e68faa0a8c9acdfc4e9
Alexis Dominguez <alexis.dominguez@truehome.com.mx>
2022-10-09 20:55:43 -0600

    Feat: Added update and delete notes endpoints

--- app/notes/views.py
+++ app/notes/views.py
@@ -28,3 +28,19 @@ class NotesDetails(APIView):
             serializer = NotesSerializer(note)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response("The requested note was not found or does not exists.", status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        note = Notes.objects.filter(pk=pk).first()
+        serializer = NotesSerializer(note, request.data)
+        if (note and serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        note = Notes.objects.filter(pk=pk).first()
+        if (note):
+            serializer = NotesSerializer(note)
+            note.delete()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response("The requested note was not found or does not exists.", status=status.HTTP_404_NOT_FOUND)
