b24b3f6d32eee70334fe613b5af1f73ea993896a
Alexis Dominguez <alexis.dominguez@truehome.com.mx>
2022-10-16 21:09:13 -0600

    Feat: Added documentation for notes and notebooks views

--- app/notebooks/views.py
+++ app/notebooks/views.py
@@ -9,6 +9,14 @@ from .serializers import NotebooksSerializer
 
 class NotebooksList(APIView):
     def post(self, request):
+        """Creates a new notebook
+
+        Args:
+             request (any): Data that cames from the request
+
+        Returns:
+           Response: returns serializer data and http status
+        """
         serializer = NotebooksSerializer(data=request.data)
         if (serializer.is_valid()):
             serializer.save()
@@ -16,6 +24,14 @@ class NotebooksList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
+        """Retrieves all notebooks
+
+        Args:
+            request (any): Data that cames from the request
+
+        Returns:
+            Response: returns serializer data and http status
+        """
         notebooks = Notebooks.objects.all().order_by('created_at')
         serializer = NotebooksSerializer(notebooks, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -23,6 +39,15 @@ class NotebooksList(APIView):
 
 class NotebooksDetails(APIView):
     def get(self, request, pk):
+        """Retrieves a specific notebook by ID
+
+        Args:
+            request (any): Data that cames from the request
+            pk (int): Public key (ID) of the notebook to get
+
+        Returns:
+            Response: returns serializer data and http status
+        """
         notebook = Notebooks.objects.filter(pk=pk).first()
         if (notebook):
             serializer = NotebooksSerializer(notebook)
@@ -30,6 +55,15 @@ class NotebooksDetails(APIView):
         return Response("The requested notebook was not found or does not exists.", status=status.HTTP_404_NOT_FOUND)
 
     def put(self, request, pk):
+        """Updates a specific notebook by ID
+
+        Args:
+            request (any): Data that cames from the request
+            pk (int): Public key (ID) of the notebook to get
+
+        Returns:
+            Response: returns serializer data and http status
+        """
         notebook = Notebooks.objects.filter(pk=pk).first()
         serializer = NotebooksSerializer(notebook, request.data)
 
@@ -42,6 +76,15 @@ class NotebooksDetails(APIView):
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
+        """Deletes a specific notebook by ID
+
+        Args:
+            request (any): Data that cames from the request
+            pk (int): Public key (ID) of the notebook to get
+
+        Returns:
+            Response: returns serializer data and http status
+        """
         notebook = Notebooks.objects.filter(pk=pk).first()
         if (notebook):
             serializer = NotebooksSerializer(notebook)
--- app/notes/views.py
+++ app/notes/views.py
@@ -9,6 +9,14 @@ from .serializers import NotesSerializer
 
 class NotesList(APIView):
     def post(self, request):
+        """Post method to create notes
+
+        Args:
+            request (any): Data that cames from the request
+
+        Returns:
+            Response: returns serializer data and http status
+        """
         serializer = NotesSerializer(data=request.data)
         if (serializer.is_valid()):
             serializer.save()
@@ -16,6 +24,14 @@ class NotesList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
+        """Gets all notes
+
+        Args:
+            request (any): Data that cames from the request
+
+        Returns:
+             Response: returns serializer data and http status
+        """
         notes = Notes.objects.all().order_by('created_at')
         serializer = NotesSerializer(notes, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -23,6 +39,15 @@ class NotesList(APIView):
 
 class NotesDetails(APIView):
     def get(self, request, pk):
+        """Gets a specific note by ID
+
+        Args:
+            request (any): Data that cames from the request
+            pk (int): Public key (ID) of the note to get
+
+        Returns:
+            Response: returns serializer data and http status
+        """
         note = Notes.objects.filter(pk=pk).first()
         if (note):
             serializer = NotesSerializer(note)
@@ -30,6 +55,15 @@ class NotesDetails(APIView):
         return Response("The requested note was not found or does not exists.", status=status.HTTP_404_NOT_FOUND)
 
     def put(self, request, pk):
+        """Updates a specific note by ID
+
+        Args:
+            request (any): Data that cames from the request
+            pk (int): Public key (ID) of the note to get
+
+        Returns:
+            Response: returns serializer data and http status
+        """
         note = Notes.objects.filter(pk=pk).first()
         serializer = NotesSerializer(note, request.data)
 
@@ -42,6 +76,15 @@ class NotesDetails(APIView):
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
+        """Deletes a specific note by ID
+
+        Args:
+            request (any): Data that cames from the request
+            pk (int): Public key (ID) of the note to get
+
+        Returns:
+            Response: returns serializer data and http status
+        """
         note = Notes.objects.filter(pk=pk).first()
         if (note):
             serializer = NotesSerializer(note)
@@ -52,6 +95,15 @@ class NotesDetails(APIView):
 
 class NotesByNotebook(APIView):
     def get(self, request, notebook_id):
+        """Gets all notes related to a notebook by Notebook ID
+
+        Args:
+            request (any): Data that cames from the request
+            notebook_id (int): Public key (ID) of the notebook
+
+        Returns:
+            Response: returns serializer data and http status
+        """
         notes = Notes.objects.filter(
             notebook=notebook_id).order_by('created_at')
         serializer = NotesSerializer(notes, many=True)
