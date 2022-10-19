63da3cb37a9848cfba62702cd841913297ef52d9
Alexis Dominguez <alexis.dominguez@truehome.com.mx>
2022-10-19 12:52:35 -0600

    Fix: fixed notebooks app name

--- app/notebooks/apps.py
+++ app/notebooks/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NotebooksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.notebooks'
+    name = 'app.notebooks'
